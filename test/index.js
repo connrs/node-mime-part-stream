37039be0902b41f25a787ea4e07ae153f4f41833
Paul Connolley <paul.connolley@gmail.com>
2013-08-28 22:42:18 +0100

    Issue #3 - Refactor the module to expose read and write streams

--- test/index.js
+++ test/index.js
@@ -5,60 +5,61 @@ var mimePartStream = require('../');
 test('Text plain quoted printable', function (t) {
   var output = '';
   var expected = 'Content-Type: text/plain; charset=UTF-8\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\nHello world!\r\n';
-  var stream = mimePartStream({type: 'text/plain; charset=UTF-8', transferEncoding: 'quoted-printable'});
+  var mimePart = mimePartStream({type: 'text/plain; charset=UTF-8', transferEncoding: 'quoted-printable'});
 
-  stream.on('end', function () {
+  mimePart.readStream.on('end', function () {
     t.equal(output, expected);
     t.end();
   });
-  stream.on('data', function (data) {
+  mimePart.readStream.on('data', function (data) {
+    console.log(data.toString());
     output += data.toString();
   });
-  stream.end('Hello world!');
+  mimePart.writeStream.end('Hello world!');
 });
 
 test('Text html quoted printable', function (t) {
   var output = '';
   var expected = 'Content-Type: text/html; charset=UTF-8\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\n<html><body><div class=3D"qp">Hello world!</div></body></html>\r\n';
-  var stream = mimePartStream({type: 'text/html; charset=UTF-8', transferEncoding: 'quoted-printable'});
+  var mimePart = mimePartStream({type: 'text/html; charset=UTF-8', transferEncoding: 'quoted-printable'});
 
-  stream.on('end', function () {
+  mimePart.readStream.on('end', function () {
     t.equal(output, expected);
     t.end();
   });
-  stream.on('data', function (data) {
+  mimePart.readStream.on('data', function (data) {
     output += data.toString();
   });
-  stream.end('<html><body><div class="qp">Hello world!</div></body></html>');
+  mimePart.writeStream.end('<html><body><div class="qp">Hello world!</div></body></html>');
 });
 
 test('Image/png base64 encoding', function (t) {
   var output = '';
   var expected = 'Content-Type: image/png\r\nContent-Transfer-Encoding: base64\r\n\r\nWU9VUkVBTExZU0hPVUxEUFJFVEVORFRIQVRUSElTSVNBTklNQUdFIA==\r\n';
-  var stream = mimePartStream({type: 'image/png', transferEncoding: 'base64'});
+  var mimePart = mimePartStream({type: 'image/png', transferEncoding: 'base64'});
 
-  stream.on('end', function () {
+  mimePart.readStream.on('end', function () {
     t.equal(output, expected);
     t.end();
   });
-  stream.on('data', function (data) {
+  mimePart.readStream.on('data', function (data) {
     output += data.toString();
   });
-  stream.end('YOUREALLYSHOULDPRETENDTHATTHISISANIMAGE ');
+  mimePart.writeStream.end('YOUREALLYSHOULDPRETENDTHATTHISISANIMAGE ');
 });
 
 test('Content-Disposition header', function (t) {
   var output = '';
   var expected = 'Content-Type: image/png\r\nContent-Disposition: attachment; filename=potato.jpg\r\nContent-Transfer-Encoding: base64\r\n\r\nWU9VUkVBTExZU0hPVUxEUFJFVEVORFRIQVRUSElTSVNBTklNQUdFIA==\r\n';
-  var stream = mimePartStream({type: 'image/png', transferEncoding: 'base64', disposition: 'attachment; filename=potato.jpg'});
+  var mimePart = mimePartStream({type: 'image/png', transferEncoding: 'base64', disposition: 'attachment; filename=potato.jpg'});
 
-  stream.on('end', function () {
+  mimePart.readStream.on('end', function () {
     t.equal(output, expected);
     t.end();
   });
-  stream.on('data', function (data) {
+  mimePart.readStream.on('data', function (data) {
     output += data.toString();
   });
-  stream.end('YOUREALLYSHOULDPRETENDTHATTHISISANIMAGE ');
+  mimePart.writeStream.end('YOUREALLYSHOULDPRETENDTHATTHISISANIMAGE ');
   
 });
